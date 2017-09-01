f96899595b49d0de75aed14f32801b73257c0fe6
Adham Kurniawan <dmkrnwn@gmail.com>
2017-09-01 15:44:24 +0700

    update show image path

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Food Lover
+
 # README
 
 This README would normally document whatever steps are necessary to get the
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,6 +13,6 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
 
   validates :title, :description, :image, presence: true
-  has_attached_file :image, styles: { medium: "400x400#", thumb: "100x100>" }, :storage => :cloudinary, :path => ":filename"
+  has_attached_file :image, styles: { medium: "400x400#", thumb: "100x100>" }, :storage => :cloudinary, :path => "food-lover/recipes/:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
